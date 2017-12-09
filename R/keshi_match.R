9d7aa80f214fb4dff991d03f56c0d92f935a0a5d
superjcd <929760274@qq.com>
2017-12-09 17:10:58 +0800

    delete redudent file

--- R/keshi_match.R
+++ R/keshi_match.R
@@ -2,10 +2,11 @@ keshi_match<-function(x){
   if(stringr::str_detect(x,pattern = 'icu|ICU')){ICU
   }else {
     stringr::str_match(x,
-             pattern = '[^[:digit:]^[:punct:]^[a-zA-Z]^[东西南北]]{2,20}')
+             pattern = '[^[:digit:]^[:punct:]^[a-zA-Z]^[东西南北]
+             ^[一二三]]{2,20}')
       }
-  
+
 }
 
 
-keshi_match(targets[200:280])
+
